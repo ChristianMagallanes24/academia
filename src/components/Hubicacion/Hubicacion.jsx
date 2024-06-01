2142367a5f186244117bbc883a12e502dbada37c
christian magallanes <desarrollowebfront@gmail.com>
2024-06-01 20:43:40 -0300

    holis

--- src/components/Hubicacion/Hubicacion.jsx
+++ src/components/Hubicacion/Hubicacion.jsx
@@ -8,7 +8,7 @@ const Hubicacion = () => {
       <div className="mapa">
         <iframe
           title="Ubicación"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d821.763749005696!2d-56.2831974303345!3d-34.5268345934276!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a1b423459fde7d%3A0xb4641d142ea767a8!2sRuta%2062%20411%2C%2090000%20Canelones%2C%20Departamento%20de%20Canelones!5e0!3m2!1ses-419!2suy!4v1717284669547!5m2!1ses-419!2suy" // Inserta aquí tu URL de Google Maps
+          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d244.31123554140035!2d-56.28254321522207!3d-34.52703528936649!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95a1b4234f94a441%3A0x3abfd1ad5de74ffe!2sJos%C3%A9%20Batlle%20y%20Ord%C3%B3%C3%B1ez%20418%2C%2090000%20Canelones%2C%20Departamento%20de%20Canelones!5e0!3m2!1ses-419!2suy!4v1717285383887!5m2!1ses-419!2suy" // Inserta aquí tu URL de Google Maps
           width="100%"
           height="450"
           style={{ border: 0 }}
